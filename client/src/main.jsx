ff8e07d29c830b1684ea71c28d896fd3fa261dec
Atanas Bobev <bobevatanas@gmail.com>
2023-08-26 16:34:34 +0300

    added an ingenious language handlers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,18 @@
-import { render } from 'preact'
-import { App } from './app.jsx'
-import './index.css'
+import { render } from "preact";
+import { App } from "./app.jsx";
+import "./index.css";
+import translate  from "./utils/languagesHandler.js";
 
-render(<App />, document.getElementById('app'))
+//wait for the language data to load before rendering the app
+const waitForLanguageData = () => {
+    if (translate("label.logoText") === "label.logoText") {
+        console.log("Waiting for language data. Checking every 100ms...");
+        setTimeout(waitForLanguageData, 100);
+    } else {
+        console.log("Language data loaded");
+        render(<App />, document.getElementById("app"));
+
+    }
+};
+waitForLanguageData();
+
